e1bddbb3519c49c99e6d2f2f15db899f7b57a0fb
Stefan Csizmazia <s.csizmazia@gmail.com>
2014-03-28 12:00:51 +0100

    first wysiwyg stuff: font family, font size (still buggy)

--- js/snte.js
+++ js/snte.js
@@ -1,5 +1,6 @@
 var snteWorkspace;
 var snteWorkspaceElements = {};
+var snteWorkspaceFocusedElement;
 
 
 $(document).ready(function() {
@@ -10,6 +11,11 @@ $(document).ready(function() {
 
 function snte_bootstrap() {
   snteWorkspace = $("div#snte-workspace");
+  /*snteWorkspace.click(function(e) {
+    console.log("click on workspace");
+    snteWorkspaceFocusedElement = snteWorkspace;
+    e.preventDefault();
+  });*/
 
   $("div#snte-menu-add-element ul.dropdown-menu li a").click(function(e) {
     console.log("Menu Item");
@@ -19,6 +25,81 @@ function snte_bootstrap() {
     
     e.preventDefault();
   });
+
+  $("button#snte-menu-font-bold").click(function(e) {
+    if(snteWorkspaceFocusedElement != null) {
+      snteWorkspaceFocusedElement.focus();
+      document.execCommand("styleWithCSS", false, "true");
+      document.execCommand("bold", false, null);
+    }
+  });
+  $("button#snte-menu-font-italic").click(function(e) {
+    if(snteWorkspaceFocusedElement != null) {
+      snteWorkspaceFocusedElement.focus();
+      document.execCommand("styleWithCSS", false, "true");
+      document.execCommand("italic", false, null);
+    }
+  });
+  $("button#snte-menu-font-underline").click(function(e) {
+    if(snteWorkspaceFocusedElement != null) {
+      snteWorkspaceFocusedElement.focus();
+      document.execCommand("styleWithCSS", false, "true");
+      document.execCommand("underline", false, null);
+    }
+  });
+  $("button#snte-menu-font-strikethrough").click(function(e) {
+    if(snteWorkspaceFocusedElement != null) {
+      snteWorkspaceFocusedElement.focus();
+      document.execCommand("styleWithCSS", false, "true");
+      document.execCommand("strikeThrough", false, null);
+    }
+  });
+  $("div#snte-menu-font-family ul.dropdown-menu li a").click(function(e) {
+    $("div#snte-menu-font-family button span.value").text($(this).text());
+    $("div#snte-menu-font-family button").data("value", $(this).data("value"));
+
+    snte_wysiwyg_apply_font_family();
+
+    e.preventDefault();
+  });
+  $("div#snte-menu-font-size ul.dropdown-menu li a").click(function(e) {
+    $("div#snte-menu-font-size button span.value").text($(this).text());
+    $("div#snte-menu-font-size button").data("value", $(this).data("value"));
+
+   snte_wysiwyg_apply_font_size();
+  });
+  $("div#snte-menu-font-color ul.dropdown-menu li a").click(function(e) {
+    document.execCommand("fontColor", false, $(this).data("value"));
+    snteWorkspaceFocusedElement.focus();
+    e.preventDefault();
+  });
+}
+
+function snte_wysiwyg_apply_font() {
+  snte_wysiwyg_apply_font_family();
+  snte_wysiwyg_apply_font_size();
+}
+
+function snte_wysiwyg_apply_font_family() {
+  if(snteWorkspaceFocusedElement != null) {
+    console.log("apply font family");
+    console.log(snteWorkspaceFocusedElement);
+    console.log($("div#snte-menu-font-family button").data("value"));
+    document.execCommand("styleWithCSS", false, "true");
+    document.execCommand("fontName", false, $("div#snte-menu-font-family button").data("value"));
+    snteWorkspaceFocusedElement.focus();
+  }
+}
+
+function snte_wysiwyg_apply_font_size() {
+  if(snteWorkspaceFocusedElement != null) {
+    console.log("apply font size");
+    console.log(snteWorkspaceFocusedElement);
+    console.log($("div#snte-menu-font-size button").data("value"));
+    document.execCommand("styleWithCSS", false, "true");
+    document.execCommand("fontSize", false, $("div#snte-menu-font-size button").data("value"));
+    snteWorkspaceFocusedElement.focus();
+  }
 }
 
 function snte_workspace_add_item(type) {
@@ -42,7 +123,7 @@ function snte_workspace_add_item(type) {
 
 function snte_workspace_add_table() {
   var nextId = snte_generate_element_id();
-  var newElement = $("<div class=\"snte-element-table\" id=\"snte-element-"+nextId+"\"></div>");
+  var newElement = $("<div class=\"snte-element snte-element-table\" id=\"snte-element-"+nextId+"\"></div>");
   newElement.handsontable({
     startRows: 10,
     startCols: 10,
@@ -89,8 +170,8 @@ function snte_workspace_add_table() {
 function snte_workspace_add_text() {
   var nextId = snte_generate_element_id();
 
-  var newElement = $("<div class=\"snte-element-text\" id=\"snte-element-"+nextId+"\"></div>");
-  newElement.editable(function(value, settings) {
+  var newElement = $("<div class=\"snte-element snte-element-text\" id=\"snte-element-"+nextId+"\" contenteditable=\"true\"></div>");
+  /*newElement.editable(function(value, settings) {
       if(value == "") {
         value = " ";
       }
@@ -98,7 +179,7 @@ function snte_workspace_add_text() {
     }, {
     onblur: "submit",
     type: "textarea"
-  });
+  });*/
   
   var newElementContainer = $("<div class=\"snte-element-container\" id=\"snte-element-"+nextId+"\"><div class=\"snte-element-control-handles snte-hidden\"><div class=\"snte-element-drag-handle\"></div><div class=\"snte-element-delete\"></div></div></div>");
   newElementContainer.mouseover(function() {
@@ -117,10 +198,19 @@ function snte_workspace_add_text() {
   newElementContainer.draggable({
     handle: $("div.snte-element-drag-handle", newElementContainer)
   });
+  newElement.focus(function(e) {
+    snteWorkspaceFocusedElement = $(this);
+    $(this).addClass("snte-highlighted");
+  });
+  newElement.blur(function(e) {
+    $(this).removeClass("snte-highlighted");
+  });
 
   snteWorkspaceElements[nextId] = newElement;
   newElement.appendTo(newElementContainer);
   newElementContainer.appendTo(snteWorkspace);
+  newElement.focus();
+  snte_wysiwyg_apply_font();
 }
 
 function snte_workspace_add_header() {
